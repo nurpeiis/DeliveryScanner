f0ac7a2a240c3d7ea06eb4a4cab50c26c33ebb66
Nurpeiis Baimukan <nb2577@nyu.edu>
2018-11-09 14:27:35 +0400

    Added Database, app search

---
+++ DeliveryScanner/settings/prod.py
@@ -0,0 +1,18 @@
+from DeliveryScanner.settings.base import *
+
+#Overwrite base settings here
+# Read secret key from a file
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+try:
+    from DeliveryScanner.settings.base import *
+except:
+    pass
