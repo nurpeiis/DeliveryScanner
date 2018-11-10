8b10a60728af4cef292451b12538dda430894eb3
Nurpeiis Baimukan <nb2577@nyu.edu>
2018-11-10 04:57:39 +0400

    clean data

--- search/views.py
+++ search/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView
-from haystack.query import SearchQuerySet
+from django.db.models import Q
 from .models import Pricedata
 
 from django.shortcuts import render_to_response
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
-  
+    def get(self, request):
+        query = request.GET.get("get")
+        qs = Pricedata.objects.all()
+        if query is not None:
+            qs = qs.filter( Q(dishname_icontains=search_query) | Q(restaurant_icontains=search_query) )
+        return render(request, self.template_name, {'results': qs} )
     """
     def get_queryset(self):
         query = self.request.GET.get('q')
